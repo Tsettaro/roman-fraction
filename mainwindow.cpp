544994b29752f62d7770cb39af38db224d54039c
ParkSuMin <tsettaro@paksurion.ru>
2024-05-12 00:04:04 +0300

    Initial commit

---
+++ mainwindow.cpp
@@ -0,0 +1,94 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+#include "Roman.h"
+#include <regex>
+
+string pattern = "^-?M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})";
+
+int check_correct (const string &text){
+    if (text == "") return 0;
+    regex roman_fraction(pattern);
+    if (regex_match(text, roman_fraction)) return 1;
+    return 0;
+}
+
+int MainWindow::check_correct_input(){
+    return (check_correct(ui->textEdit->toPlainText().toUtf8().constData()) &&
+            check_correct(ui->textEdit_2->toPlainText().toUtf8().constData()) &&
+            check_correct(ui->textEdit_5->toPlainText().toUtf8().constData()) &&
+            check_correct(ui->textEdit_6->toPlainText().toUtf8().constData()) == 1);
+}
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow(){
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked(){
+    if (check_correct_input()){
+        RomanFraction *fr1 = new RomanFraction(ui->textEdit->toPlainText().toUtf8().constData(),
+                          ui->textEdit_2->toPlainText().toUtf8().constData());
+        RomanFraction *fr2 = new RomanFraction(ui->textEdit_5->toPlainText().toUtf8().constData(),
+                          ui->textEdit_6->toPlainText().toUtf8().constData());
+        RomanFraction fr3 = *(fr1) + *(fr2);
+        ui->label_3->setText(QString::fromStdString(fr3.fraction()));
+        delete fr1;
+        delete fr2;
+    } else{
+        QMessageBox::critical(this, "ВНИМАНИЕ!", "Ошибка при вводе дробей!");
+    }
+}
+
+void MainWindow::on_pushButton_2_clicked(){
+    if (check_correct_input()){
+        RomanFraction *fr1 = new RomanFraction(ui->textEdit->toPlainText().toUtf8().constData(),
+                                               ui->textEdit_2->toPlainText().toUtf8().constData());
+        RomanFraction *fr2 = new RomanFraction(ui->textEdit_5->toPlainText().toUtf8().constData(),
+                                               ui->textEdit_6->toPlainText().toUtf8().constData());
+        RomanFraction fr3 = *(fr1) - *(fr2);
+        ui->label_3->setText(QString::fromStdString(fr3.fraction()));
+        delete fr1;
+        delete fr2;
+    } else{
+        QMessageBox::critical(this, "ВНИМАНИЕ!", "Ошибка при вводе дробей!");
+    }
+}
+
+
+void MainWindow::on_pushButton_3_clicked(){
+    if (check_correct_input()){
+        RomanFraction *fr1 = new RomanFraction(ui->textEdit->toPlainText().toUtf8().constData(),
+                                               ui->textEdit_2->toPlainText().toUtf8().constData());
+        RomanFraction *fr2 = new RomanFraction(ui->textEdit_5->toPlainText().toUtf8().constData(),
+                                               ui->textEdit_6->toPlainText().toUtf8().constData());
+        RomanFraction fr3 = *(fr1) * *(fr2);
+        ui->label_3->setText(QString::fromStdString(fr3.fraction()));
+        delete fr1;
+        delete fr2;
+    } else {
+        QMessageBox::critical(this, "ВНИМАНИЕ!", "Ошибка при вводе дробей!");
+    }
+}
+
+
+void MainWindow::on_pushButton_4_clicked(){
+    if (check_correct_input()){
+        RomanFraction *fr1 = new RomanFraction(ui->textEdit->toPlainText().toUtf8().constData(),
+                                               ui->textEdit_2->toPlainText().toUtf8().constData());
+        RomanFraction *fr2 = new RomanFraction(ui->textEdit_5->toPlainText().toUtf8().constData(),
+                                               ui->textEdit_6->toPlainText().toUtf8().constData());
+        RomanFraction fr3 = *(fr1) / *(fr2);
+        ui->label_3->setText(QString::fromStdString(fr3.fraction()));
+        delete fr1;
+        delete fr2;
+    } else {
+        QMessageBox::critical(this, "ВНИМАНИЕ!", "Ошибка при вводе дробей!");
+    }
+}
+
---
+++ roman.cpp
@@ -0,0 +1,80 @@
+#include "Roman.h"
+
+int gcd (int a, int b) {
+    while (b) {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
+
+int lcm (int a, int b) {
+    return a / gcd (a, b) * b;
+}
+
+ostream& operator<<(ostream& os, RomanFraction& fraction){
+    os << fraction.numerator << "/" << fraction.denominator;
+    return os;
+}
+
+string RomanFraction::fraction(){
+    if (numerator == "") return "NULL";
+    return numerator + "/" + denominator;
+}
+
+int RomanFraction::romanToArabic(string &roman){
+    int arabic = 0, i = 0;
+    if (roman[0] == '-') i = 1;
+    for (; i < roman.size(); i++) {
+        if (i + 1 < roman.size() && romanToArabicMap[roman[i]] < romanToArabicMap[roman[i + 1]]) {
+            arabic -= romanToArabicMap[roman[i]];
+        } else {
+            arabic += romanToArabicMap[roman[i]];
+        }
+    }
+    if (roman[0] == '-') return -arabic;
+    return arabic;
+}
+
+string RomanFraction::arabicToRoman(int number) {
+    string result = "";
+    int flag = 0;
+    if (number < 0){
+        number*=(-1);
+        flag = 1;
+    }
+    for (auto it = arabicToRomanMap.rbegin(); it != arabicToRomanMap.rend(); ++it) {
+        while (number >= it->first) {
+            result += it->second;
+            number -= it->first;
+        }
+    }
+    if (flag) return '-' + result;
+    return result;
+}
+
+RomanFraction RomanFraction::operator +(RomanFraction& other){
+    int um = lcm(abs(romanToArabic(denominator)), abs(romanToArabic(other.denominator)));
+    int n_ar = romanToArabic(numerator) * (um / abs(romanToArabic(denominator))),
+        o_ar = romanToArabic(other.numerator) * (um / abs(romanToArabic(other.denominator)));
+    return RomanFraction(n_ar + o_ar, um);
+}
+
+RomanFraction RomanFraction::operator -(RomanFraction& other){
+    int um = lcm(abs(romanToArabic(denominator)), abs(romanToArabic(other.denominator)));
+    int n_ar = romanToArabic(numerator) * (um / abs(romanToArabic(denominator))),
+        o_ar = romanToArabic(other.numerator) * (um / abs(romanToArabic(other.denominator)));
+    return RomanFraction(n_ar - o_ar, um);
+}
+
+RomanFraction RomanFraction::operator *(RomanFraction& other){
+    int n_ar = romanToArabic(numerator) * abs(romanToArabic(other.numerator)),
+        o_ar = romanToArabic(other.denominator) * abs(romanToArabic(denominator));
+    return RomanFraction(n_ar, o_ar);
+}
+
+RomanFraction RomanFraction::operator /(RomanFraction& other){
+    int n_ar = romanToArabic(numerator) * abs(romanToArabic(other.denominator)),
+        o_ar = romanToArabic(other.numerator) * abs(romanToArabic(denominator));
+    return RomanFraction(n_ar, o_ar);
+}
