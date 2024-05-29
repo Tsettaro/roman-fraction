b1c50c54b1df497dcaa355060147a5d3b6dad805
ParkSuMin <tsettaro@paksurion.ru>
2024-05-29 12:22:53 +0300

    Little fix in Roman.h
    Remove depend from map

--- include/Roman.h
+++ include/Roman.h
@@ -1,26 +1,11 @@
 #ifndef ROMAN_H
 #define ROMAN_H
 #include <string>
-#include <map>
 
 using namespace std;
 
 class RomanFraction{
 private:
-    map<int, string> arabicToRomanMap = {
-        {1, "I"},
-        {4, "IV"},
-        {5, "V"},
-        {9, "IX"},
-        {10, "X"},
-        {40, "XL"},
-        {50, "L"},
-        {90, "XC"},
-        {100, "C"},
-        {400, "CD"},
-        {500, "D"},
-        {1000, "M"}
-    };
 
     int arabicValues[13] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
     string romanValues[13] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
